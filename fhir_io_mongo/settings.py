16b53cecc148a12f3c4f8a20a5bd2c6190386ab4
Mark Scrimshire <ekivemark>
2016-02-16 11:09:20 -0500

    Attempting to integrate Docker

---
+++ fhir_io_mongo/settings.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim: ai ts=4 sts=4 et sw=4
+
+"""
+BlueButtonFHIR_API
+FILE: settings
+Created: 1/10/16 6:56 PM
+
+
+"""
+__author__ = 'Mark Scrimshire:@ekivemark'
